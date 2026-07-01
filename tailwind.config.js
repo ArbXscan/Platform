b4ae0103421862c70b30e8e8954f5247b47b5a09
ProArbx <arbxscan@gmail.com>
2026-07-01 12:23:19 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
       keyframes: {
-        marquee: {
-          "0%": {
-            transform: "translateX(0)",
-          },
-          "100%": {
-            transform: "translateX(-50%)",
-          },
-        },
+        marquee: { from: { transform: "translateX(0)" }, to: { transform: "translateX(-50%)" } },
       },
-
-      animation: {
-        marquee: "marquee 25s linear infinite",
-      },
+      animation: { marquee: "marquee 28s linear infinite" },
     },
   },
-
   plugins: [],
 }
